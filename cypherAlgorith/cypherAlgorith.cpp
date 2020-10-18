1c5dbc0244d62d4738e3e9e81a2d0b794c3ae6dc
Cezary Wakulinski <cezarywakulinskidev@gmail.com>
2020-10-18 21:25:26 +0200

    Finished version 1.0

--- cypherAlgorith/cypherAlgorith.cpp
+++ cypherAlgorith/cypherAlgorith.cpp
@@ -23,83 +23,45 @@ int AdujstIntToBound(int int_to_adjust, int min, int max) {
 	return int_to_adjust;
 }
 
-int GetIntIndexDigit(int index, unsigned int key) {
+int GetIntIndexDigit(int index, unsigned int integer) {
 	unsigned int key_length = 1;
 
-	while (key / pow(10, key_length) >= 1) {
+	while (integer / pow(10, key_length) >= 1) {
 		key_length++;
 	};
 
 	unsigned int key_divider = pow(10, key_length - index);
 
-	return (1 >= key_divider) ? 0 : floor((key % key_divider) / (key_divider / 10));
+	return (1 >= key_divider) ? 0 : floor((integer % key_divider) / (key_divider / 10));
 }
 
+int main() {
+	std::cout << "Welcome!" << std::endl << "Please select which operation do you wanna perform:" << double_endl;
+	std::cout << "[1] -> Encodeing" << std::endl;
+	std::cout << "[2] -> Decodeing" << std::endl;
 
-void encriptMessage() {
-	std::cout << "Please, enter input message" << std::endl;
+	int operation_type {};
+	std::cin >> operation_type;
+	bool is_encription = operation_type == 1;
+
+	std::cout << std::endl << "Please, enter input message" << std::endl;
+	std::cin.ignore();
 
 	std::string user_input_msg;
 	std::getline(std::cin, user_input_msg);
 
-	/* Convert input variable pointer into decimal */
-	std::stringstream ss;
-	ss << &user_input_msg;
-	std::string hex_key = ss.str().replace(0, 2, "").substr(0, kMaxKeyLength);
-	unsigned int decimal_key = std::stoll(hex_key, nullptr, 16);
-
-	/* Generate random number between [0-kMaxCharCodePadding-1] */
-	/* Range is decreased by 1 becouse 0 and maxValue would gave the same resoult */
-	int char_value_padding = decimal_key % (kMaxCharCodePadding - 1);
-	std::string output_message;
-
-	for (int index = 0; index < user_input_msg.length(); index++) {
-		int char_asci_val = int(user_input_msg[index]);
-
-		/* Input validation */
-		if (char_asci_val > 126 || char_asci_val < 32) {
-			std::cout << "Bad input" << std::endl;
-			errno = 1;
-			return;
-		}
-		else {
-			/* Add general code padding */
-			char_asci_val += char_value_padding;
-
-			/* Add index and index+1 padding squared */
-			for (int indexValue : {index, index + 1}) {
-				unsigned int inted_to_get = AdujstIntToBound(indexValue, 0, kMaxKeyLength - 1);
-
-				char_asci_val += pow(
-						GetIntIndexDigit(inted_to_get, decimal_key)
-					, 2);
-			}
-
-
-			/* Make sure new char is in bounds*/
-			output_message += char(AdujstIntToBound(char_asci_val, 32, 126));
-
-		}
+	unsigned int message_key {};
+	if (is_encription) {
+		std::stringstream ss;
+		ss << &user_input_msg;
+		std::string hex_key = ss.str().replace(0, 2, "").substr(0, kMaxKeyLength);
+		message_key = std::stoll(hex_key, nullptr, 16);
+	}
+	else {
+		std::cout << std::endl << "Please, enter decription code" << std::endl;
+		std::cin >> message_key;
 	}
 
-
-	std::cout << "Output message:" << std::endl << output_message << double_endl;
-	std::cout << "Decript key:" << std::endl << decimal_key << std::endl;
-}
-
-void decriptMessage() {
-	std::cout << "Please, enter input message" << std::endl;
-
-	std::string user_input_msg;
-	std::getline(std::cin, user_input_msg);
-
-	std::cout << "Please, enter decription code" << std::endl;
-
-	int message_key;
-	std::cin >> message_key;
-
-	/* Generate random number between [0-kMaxCharCodePadding-1] */
-	/* Range is decreased by 1 becouse 0 and maxValue would gave the same resoult */
 	int char_value_padding = message_key % (kMaxCharCodePadding - 1);
 	std::string output_message;
 
@@ -109,50 +71,36 @@ void decriptMessage() {
 		/* Input validation */
 		if (char_asci_val > 126 || char_asci_val < 32) {
 			std::cout << "Bad input" << std::endl;
-			errno = 1;
-			return;
+			return 1;
 		}
 		else {
+			int key_padding_value {};
+
+			/* Add general code padding */
+			key_padding_value += char_value_padding;
+
 			/* Add index and index+1 padding squared */
 			for (int indexValue : {index, index + 1}) {
 				unsigned int inted_to_get = AdujstIntToBound(indexValue, 0, kMaxKeyLength - 1);
 
-				char_asci_val -= pow(
+				key_padding_value += pow(
 					GetIntIndexDigit(inted_to_get, message_key)
 					, 2);
 			}
 
-			/* Add general code padding */
-			char_asci_val -= char_value_padding;
-
+			/* ASCI modification */
+			char_asci_val += key_padding_value * (is_encription ? 1 : -1);
 
 			/* Make sure new char is in bounds*/
 			output_message += char(AdujstIntToBound(char_asci_val, 32, 126));
-
 		}
 	}
 
+	std::cout << std::endl << "Message:" << std::endl << output_message << double_endl;
 
-	std::cout << "Output message:" << std::endl << output_message << double_endl;
-};
-
-
-int main() {
-	std::cout << "Welcome!" << std::endl << "Please select which operation do you wanna perform:" << double_endl;
-	std::cout << "[1] -> Encodeing" << std::endl;
-	std::cout << "[2] -> Decodeing" << std::endl;
-
-	int operation_type;
-	std::cin >> operation_type;
-
-	std::cin.ignore();
-
-	if (operation_type == 1) {
-		encriptMessage();
-	}
-	else {
-		decriptMessage();
+	if (is_encription) {
+		std::cout << "Decript key:" << std::endl << message_key << std::endl;
 	}
 
-	return errno;
+	return 0;
 }
